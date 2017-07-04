99e51f909ae97d771adf343c4aef9f2892fd73e6
stfsy <s.pfaffel@gmail.com>
2017-07-04 17:49:42 +0200

    chore(assert.go): swap actual and expected args

--- assert.go
+++ assert.go
@@ -2,15 +2,15 @@ package assert
 
 import "testing"
 
-func Equal(t *testing.T, expected interface{}, actual interface{}, message string) {
-	assert(t, expected, actual, message, true)
+func Equal(t *testing.T, actual interface{}, expected interface{}, message string) {
+	assert(t, actual, expected, message, true)
 }
 
-func NotEqual(t *testing.T, expected interface{}, actual interface{}, message string) {
-	assert(t, expected, actual, message, false)
+func NotEqual(t *testing.T, actual interface{}, expected interface{}, message string) {
+	assert(t, actual, expected, message, false)
 }
 
-func assert(t *testing.T, expected interface{}, actual interface{}, message string, expectation bool) {
+func assert(t *testing.T, actual interface{}, expected interface{}, message string, expectation bool) {
 	if (expected == actual) != expectation {
 		t.Errorf("%s failed: Expected %v, but got %v", message, expected, actual)
 	}
